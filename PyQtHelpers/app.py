b5f6946b3d20bf760e1f2de1fb3647015303bc75
nhaines <scumdog.mcgee@gmail.com>
2015-03-09 19:54:32 -0700

    starting to add networking applications

--- PyQtHelpers/app.py
+++ PyQtHelpers/app.py
@@ -4,6 +4,8 @@ from PyQt4 import QtCore
 from PyQt4 import QtGui
 from PyQt4 import QtNetwork
 
+import network
+
 
 class BaseApp(QtGui.QApplication):
     def __init__(self, argv=[], organization="PyQtHelpers", name="Default"):
@@ -11,9 +13,6 @@ class BaseApp(QtGui.QApplication):
         self.setOrganizationName(organization)
         self.setApplicationName(name)
         self.onLoad()
-        self.__connect()
-
-    def __connect(self):
         self.lastWindowClosed.connect(self.onExit)
 
     def onLoad(self):
@@ -25,17 +24,24 @@ class BaseApp(QtGui.QApplication):
 
 class HttpClientApp(BaseApp):
     def __init__(self, argv=[], organization="PyQtHelpers", name="Default"):
-        self.manager = QtNetwork.QNetworkAccessManager()
+        self.client = QtNetwork.QNetworkAccessManager()
         super(HttpClientApp, self).__init__(argv, organization, name)
 
     def onLoad(self):
-        cookies = QtNetwork.QNetworkCookie.parseCookies(QtCore.QSettings().value("manager/cookies", ""))
+        cookies = QtNetwork.QNetworkCookie.parseCookies(QtCore.QSettings().value("network/client/cookies", ""))
         cookie_jar = QtNetwork.QNetworkCookieJar()
         cookie_jar.setAllCookies(cookies)
-        self.manager.setCookieJar(cookie_jar)
+        self.client.setCookieJar(cookie_jar)
 
     def onExit(self):
         cookies = QtCore.QByteArray()
-        for cookie in self.manager.cookieJar().allCookies():
+        for cookie in self.client.cookieJar().allCookies():
             cookies.append(cookie.toRawForm())
-        QtCore.QSettings().setValue("manager/cookies", cookies)
+        QtCore.QSettings().setValue("network/client/cookies", cookies)
+
+
+class HttpServerApp(BaseApp):
+    def __init__(self, argv=[], organization="PyQtHelpers", name="Default", port=80):
+        self.server = network.HttpServer()
+        super(HttpServerApp, self).__init__(argv, organization, name)
+        self.server.listen(port=port)
--- PyQtHelpers/network.py
+++ PyQtHelpers/network.py
@@ -1,6 +1,34 @@
 __author__ = 'nhaines'
 
+from PyQt4 import QtCore
 from PyQt4 import QtNetwork
 
-import models
 
+class HttpRequestHandler(QtCore.QRunnable, QtCore.QObject):
+    finished = QtCore.pyqtSignal(QtNetwork.QTcpSocket, QtCore.QByteArray)
+
+    def __init__(self, socket):
+        super(HttpRequestHandler, self).__init__()
+        self.socket = socket
+
+    def run(self):
+        request = QtNetwork.QHttpRequestHeader()
+        # data = QtCore.QByteArray()
+
+        print request.readAll()
+
+
+class HttpServer(QtNetwork.QTcpServer):
+    def __init__(self, parent=None):
+        super(HttpServer, self).__init__(parent)
+        self.__pool = QtCore.QThreadPool(self)
+        self.__connect()
+
+    def __connect(self):
+        self.newConnection.connect(self.handle_connection)
+
+    def handle_connection(self):
+        print 'handle it'
+        sock = self.nextPendingConnection()
+        runner = HttpRequestHandler(socket)
+        # self.__pool.
